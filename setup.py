d710cee348af57da9662dc413e487fcf12f26c7c
Christian Theune <ct@gocept.com>
2007-03-21 08:15:34 +0000

    - updated metadata

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="zc.sourcefactory",
-    version="0.1dev",
+    version="0.1",
+    author="Zope Corporation and Contributors",
+    author_email="zope3-dev@zope.org",
+
+    long_description="An easy way to create custom Zope 3 sources."
+
     packages=find_packages('src'),
     package_dir={'':'src'},
+
     include_package_data=True,
     install_requires=["setuptools"],
     namespace_packages=['zc'],
+
     zip_safe=False
     )
