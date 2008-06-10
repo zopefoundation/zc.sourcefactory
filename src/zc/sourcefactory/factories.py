62993d74905c5926d9f9a546935466e4b79cf666
Christian Theune <ct@gocept.com>
2008-06-10 15:04:30 +0000

    Allow subclasses of the factories to have custom constructor signatures.

--- src/zc/sourcefactory/factories.py
+++ src/zc/sourcefactory/factories.py
@@ -32,10 +32,10 @@ class BasicSourceFactory(object):
 
     zope.interface.implements(zc.sourcefactory.interfaces.ISourceFactory)
 
-    def __new__(cls):
+    def __new__(cls, *args, **kw):
         """Create the factory object and return source."""
         factory = object.__new__(cls)
-        factory.__init__()
+        factory.__init__(*args, **kw)
         return zc.sourcefactory.source.FactoredSource(factory)
 
 
