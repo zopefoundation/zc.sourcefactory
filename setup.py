f21e53857d213f513dd658680bc1082c4ee06ffc
Stephan Richter <stephan.richter@gmail.com>
2013-02-23 00:13:25 -0500

    Preparing release 1.0.0a1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def alltests():
 
 setup(
     name='zc.sourcefactory',
-    version='1.0.0a1.dev',
+    version='1.0.0a1',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     url='http://pypi.python.org/pypi/zc.sourcefactory',
