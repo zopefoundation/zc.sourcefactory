84ffc67fe31981056ebdb923b8a07cc5a993ace4
Michael Howitz <mh@gocept.com>
2018-11-07 09:22:25 +0100

    Add support for Python 3.6 and 3.7. (#2)
    * Add support for Python 3.6 and 3.7.
    * Drop support for Python 3.3 and 3.4.
    * Drop support for `python setup.py test`.
    * Add toplevel readme.
    * Pimp MANIFEST.

--- setup.py
+++ setup.py
@@ -16,36 +16,24 @@
 from setuptools import setup, find_packages
 import os.path
 
-def read_file(*args):
-    path = os.path.join(os.path.dirname(__file__), *args)
-    file_contents = open(path, 'r').read()
-    return file_contents + '\n\n'
 
-def alltests():
-    import os
-    import sys
-    import unittest
-    # use the zope.testrunner machinery to find all the
-    # test suites we've put under ourselves
-    import zope.testrunner.find
-    import zope.testrunner.options
-    here = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
-    args = sys.argv[:]
-    defaults = ["--test-path", here]
-    options = zope.testrunner.options.get_options(args, defaults)
-    suites = list(zope.testrunner.find.find_suites(options))
-    return unittest.TestSuite(suites)
+def read_file(*args):
+    path = os.path.join(*args)
+    with open(path, 'r') as f:
+        return f.read() + '\n\n'
 
 setup(
     name='zc.sourcefactory',
     version='1.1.0.dev0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
-    url='http://pypi.python.org/pypi/zc.sourcefactory',
-    keywords='zope zope3 vocabulary source factory',
-    description='An easy way to create custom Zope 3 sources.',
+    url='https://github.com/zopefoundation/zc.sourcefactory',
+    keywords='zope vocabulary source factory',
+    description='An easy way to create custom Zope sources.',
     license='ZPL 2.1',
     long_description=(
+        read_file('README.rst') +
+        '\n\n.. contents::\n\n' +
         read_file('src', 'zc', 'sourcefactory', 'README.txt') +
         read_file('src', 'zc', 'sourcefactory', 'mapping.txt') +
         read_file('src', 'zc', 'sourcefactory', 'constructors.txt') +
@@ -53,10 +41,11 @@ setup(
         read_file('src', 'zc', 'sourcefactory', 'browser', 'README.txt') +
         read_file('src', 'zc', 'sourcefactory', 'browser', 'token.txt') +
         read_file('CHANGES.txt')
-        ),
-    classifiers = [
+    ),
+    classifiers=[
         'Topic :: Software Development',
         'Framework :: Zope :: 3',
+        'Framework :: Zope :: 4',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Zope Public License',
@@ -64,17 +53,15 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        ],
-
+    ],
     packages=find_packages('src'),
-    package_dir={'':'src'},
-
+    package_dir={'': 'src'},
     include_package_data=True,
     install_requires=[
         'ZODB',
@@ -88,23 +75,16 @@ setup(
         'zope.proxy',
         'zope.publisher',
         'zope.schema',
-        ],
+    ],
     namespace_packages=['zc'],
     extras_require={
         'test': [
             'zope.component[zcml]',
+            'persistent >= 4.4.3',
             'zope.site',
             'zope.testing',
             'zope.testrunner',
             'zope.keyreference',
-            ]},
-    tests_require = [
-        'zope.component[zcml]',
-        'zope.keyreference',
-        'zope.site',
-        'zope.testing',
-        'zope.testrunner',
-        ],
-    test_suite = '__main__.alltests',
+        ]},
     zip_safe=False,
-    )
+)
